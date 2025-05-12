a38291ed387182944650d6f60707802d35f27308
Rich Higgins <hello@iamrichhiggins.com>
2025-05-12 16:13:20 +0100

    ++ ts ignores

--- nextjs-app/app/test/page.tsx
+++ nextjs-app/app/test/page.tsx
@@ -1,6 +1,18 @@
 // src/app/test/page.tsx
 import { sanityFetch } from "@/sanity/lib/live";
+import {
+  PortableTextMarkDefinition,
+  ArbitraryTypedObject,
+  PortableTextSpan,
+} from "@portabletext/types";
 import { PortableText, PortableTextBlock } from "next-sanity";
+import {
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  AwaitedReactNode,
+  ReactPortal,
+} from "react";
 
 export default async function Page() {
   const { data: posts } = await sanityFetch({
@@ -17,36 +29,101 @@ export default async function Page() {
       <br />
       <br />
       some pageBuilder components 👇
-      {posts[0].pageBuilder.map((post) =>
-        post._type === "callToAction" ? (
-          <div
-            style={{
-              margin: "1rem",
-              padding: "1rem",
-              border: "1px dashed pink",
-            }}
-          >
-            <code style={{ color: "rebeccapurple" }}>{post._type}</code>
-            <h4>{post.heading}</h4>
-            <div>{post.text}</div>
-            <button>{post.buttonText}</button>
-          </div>
-        ) : (
-          <div
-            style={{
-              margin: "1rem",
-              padding: "1rem",
-              border: "1px dashed pink",
-            }}
-          >
-            <code style={{ color: "rebeccapurple" }}>{post._type}</code>
-            <h4>{post.heading}</h4>
-            <h5>{post.subheading}</h5>
-            <br />
-            <br />
-            <PortableText value={post.content as PortableTextBlock[]} />
-          </div>
-        )
+      {posts[0].pageBuilder.map(
+        (post: {
+          _type:
+            | string
+            | number
+            | bigint
+            | boolean
+            | ReactElement<any, string | JSXElementConstructor<any>>
+            | Iterable<ReactNode>
+            | Promise<AwaitedReactNode>
+            | null
+            | undefined;
+          heading:
+            | string
+            | number
+            | bigint
+            | boolean
+            | ReactElement<any, string | JSXElementConstructor<any>>
+            | Iterable<ReactNode>
+            | Promise<AwaitedReactNode>
+            | null
+            | undefined;
+          text:
+            | string
+            | number
+            | bigint
+            | boolean
+            | ReactElement<any, string | JSXElementConstructor<any>>
+            | Iterable<ReactNode>
+            | ReactPortal
+            | Promise<AwaitedReactNode>
+            | null
+            | undefined;
+          buttonText:
+            | string
+            | number
+            | bigint
+            | boolean
+            | ReactElement<any, string | JSXElementConstructor<any>>
+            | Iterable<ReactNode>
+            | ReactPortal
+            | Promise<AwaitedReactNode>
+            | null
+            | undefined;
+          subheading:
+            | string
+            | number
+            | bigint
+            | boolean
+            | ReactElement<any, string | JSXElementConstructor<any>>
+            | Iterable<ReactNode>
+            | ReactPortal
+            | Promise<AwaitedReactNode>
+            | null
+            | undefined;
+          content: PortableTextBlock<
+            PortableTextMarkDefinition,
+            ArbitraryTypedObject | PortableTextSpan,
+            string,
+            string
+          >[];
+        }) =>
+          post._type === "callToAction" ? (
+            <div
+              style={{
+                margin: "1rem",
+                padding: "1rem",
+                border: "1px dashed pink",
+              }}
+              // @ts-ignore
+              key={post._key}
+            >
+              <code style={{ color: "rebeccapurple" }}>{post._type}</code>
+              <h4>{post.heading}</h4>
+              <div>{post.text}</div>
+              <button>{post.buttonText}</button>
+            </div>
+          ) : (
+            <div
+              style={{
+                margin: "1rem",
+                padding: "1rem",
+                border: "1px dashed pink",
+              }}
+              // @ts-ignore
+              key={post._key}
+            >
+              <code style={{ color: "rebeccapurple" }}>{post._type}</code>
+              <h4>{post.heading}</h4>
+              <h5>{post.subheading}</h5>
+              <br />
+              <br />
+              <PortableText value={post.content as PortableTextBlock[]} />
+            </div>
+          )
       )}
       <pre>
         {
