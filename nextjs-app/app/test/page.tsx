9cc179bafe7eb1f8b10affbf2079846d8981817c
Rich Higgins <hello@iamrichhiggins.com>
2025-05-12 16:37:24 +0100

    added some further checks

--- nextjs-app/app/test/page.tsx
+++ nextjs-app/app/test/page.tsx
@@ -1,18 +1,6 @@
 // src/app/test/page.tsx
 import { sanityFetch } from "@/sanity/lib/live";
-import {
-  PortableTextMarkDefinition,
-  ArbitraryTypedObject,
-  PortableTextSpan,
-} from "@portabletext/types";
 import { PortableText, PortableTextBlock } from "next-sanity";
-import {
-  ReactElement,
-  JSXElementConstructor,
-  ReactNode,
-  AwaitedReactNode,
-  ReactPortal,
-} from "react";
 
 export default async function Page() {
   const { data: posts } = await sanityFetch({
@@ -29,77 +17,16 @@ export default async function Page() {
       <br />
       <br />
       some pageBuilder components 👇
-      {posts[0].pageBuilder.map(
-        (post: {
-          _type:
-            | string
-            | number
-            | bigint
-            | boolean
-            | ReactElement<any, string | JSXElementConstructor<any>>
-            | Iterable<ReactNode>
-            | Promise<AwaitedReactNode>
-            | null
-            | undefined;
-          heading:
-            | string
-            | number
-            | bigint
-            | boolean
-            | ReactElement<any, string | JSXElementConstructor<any>>
-            | Iterable<ReactNode>
-            | Promise<AwaitedReactNode>
-            | null
-            | undefined;
-          text:
-            | string
-            | number
-            | bigint
-            | boolean
-            | ReactElement<any, string | JSXElementConstructor<any>>
-            | Iterable<ReactNode>
-            | ReactPortal
-            | Promise<AwaitedReactNode>
-            | null
-            | undefined;
-          buttonText:
-            | string
-            | number
-            | bigint
-            | boolean
-            | ReactElement<any, string | JSXElementConstructor<any>>
-            | Iterable<ReactNode>
-            | ReactPortal
-            | Promise<AwaitedReactNode>
-            | null
-            | undefined;
-          subheading:
-            | string
-            | number
-            | bigint
-            | boolean
-            | ReactElement<any, string | JSXElementConstructor<any>>
-            | Iterable<ReactNode>
-            | ReactPortal
-            | Promise<AwaitedReactNode>
-            | null
-            | undefined;
-          content: PortableTextBlock<
-            PortableTextMarkDefinition,
-            ArbitraryTypedObject | PortableTextSpan,
-            string,
-            string
-          >[];
-        }) =>
+      {posts[0].pageBuilder &&
+        posts[0].pageBuilder.map((post: any) =>
           post._type === "callToAction" ? (
             <div
+              key={post._key}
               style={{
                 margin: "1rem",
                 padding: "1rem",
                 border: "1px dashed pink",
               }}
-              // @ts-ignore
-              key={post._key}
             >
               <code style={{ color: "rebeccapurple" }}>{post._type}</code>
               <h4>{post.heading}</h4>
@@ -108,13 +35,12 @@ export default async function Page() {
             </div>
           ) : (
             <div
+              key={post._key}
               style={{
                 margin: "1rem",
                 padding: "1rem",
                 border: "1px dashed pink",
               }}
-              // @ts-ignore
-              key={post._key}
             >
               <code style={{ color: "rebeccapurple" }}>{post._type}</code>
               <h4>{post.heading}</h4>
@@ -124,7 +50,7 @@ export default async function Page() {
               <PortableText value={post.content as PortableTextBlock[]} />
             </div>
           )
-      )}
+        )}
       <pre>
         {
           //JSON.stringify(duplicatePosts, null, 2)
